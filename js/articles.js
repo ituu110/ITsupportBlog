9e8069371c16cb73536f601892412f25497dee2c
ItoYu <y.destiny.spyair@gmail.com>
2025-03-20 16:15:13 +0900

    最新記事変更

--- js/articles.js
+++ js/articles.js
@@ -1,15 +1,21 @@
 const articles = [
     {
       title: "VMwareを使用し、WindowsPCでmacOS環境を構築する(VMwareインストールまで)",
-      url: "articles/article1.html",
+      url: "articles/install-macos-on-windows-vmware.html",
       description: "WindowsPC上でmacOS環境を構築する方法を詳細に解説。VMwareのセットアップ手順付き。",
       date: "2025-03-17"
     },
     {
-      title: "Web3.0って何完全初心者でもわかるクラウドサービスの選び方｜個人・小規模チーム編？",
-      url: "articles/article2.html",
+      title: "完全初心者でもわかるクラウドサービスの選び方｜個人・小規模チーム編？",
+      url: "articles/cloud-service-selection.html",
       description: "クラウドサービス初心者が簡単に理解できる、個人や小規模チームに最適なサービス選びを徹底解説します。おすすめサービスの特徴や選定ポイントを詳しくご紹介！",
       date: "2025-03-17"
+    }
+    {
+      title: "IPAシステムアーキテクト試験とは？特徴・勉強法・近年の傾向を徹底解説",
+      url: "articles/ipa-system-architect.html",
+      description: "システムアーキテクト試験の概要からおすすめ勉強法、最新の試験傾向まで、受験に役立つ情報を提供します。",
+      date: "2025-03-20"
     }
   ];
   
